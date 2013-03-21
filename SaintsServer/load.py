d8e925068bb963325867ad50c15219d092578ac6
Family <family@Familys-iMac.local>
2013-03-21 11:29:20 -0500

    Incremental progress. Now able to get the data from the CYC site and access the rows with the schedule data.

--- SaintsServer/load.py
+++ SaintsServer/load.py
@@ -1,15 +1,19 @@
 import logging
 import webapp2
 from google.appengine.api import urlfetch
+from lxml import etree
 
 
 
 class Load(webapp2.RequestHandler):
 	def get(self):
 		logging.info("Test cron job")
-		result = urlfetch.fetch(url="http://www.cycstcharles.com/schedule.php?season=32&conference=779&division=-1&team=-1&month=3&year=2013&pfv=y&sort=date")
-		if result.status_code == 200:
-			logging.info("valid response")
+		url = urlfetch.fetch(url="http://www.cycstcharles.com/schedule.php?season=32&conference=779&division=-1&team=-1&month=3&year=2013&pfv=y&sort=date")
+		if url.status_code == 200:
+			tree = etree.HTML(url.content)
+			result = etree.tostring(tree, pretty_print=True, method="html")
+			elements = tree.xpath('//table[@class="list"]//tr')
+			logging.info(str(len(elements)))
 
 app = webapp2.WSGIApplication([('/crontask/scrape', Load)],debug=True)
 
