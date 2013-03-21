229afc7659829993a5e3b476e76d5d65b02add2c
Family <family@Familys-iMac.local>
2013-03-21 16:46:37 -0500

    Successfully able to parse the details for each game from the returned html.  Next need to marshall these values into the team object and persist to the database.

--- SaintsServer/load.py
+++ SaintsServer/load.py
@@ -8,12 +8,17 @@ from lxml import etree
 class Load(webapp2.RequestHandler):
 	def get(self):
 		logging.info("Test cron job")
-		url = urlfetch.fetch(url="http://www.cycstcharles.com/schedule.php?season=32&conference=779&division=-1&team=-1&month=3&year=2013&pfv=y&sort=date")
+		url = urlfetch.fetch(url="http://www.cycstcharles.com/schedule.php?leagueid=1&season=32&conference=779&division=797&team=5766&pfv=y&sort=date&month=999&year=999", deadline=15)
 		if url.status_code == 200:
 			tree = etree.HTML(url.content)
-			result = etree.tostring(tree, pretty_print=True, method="html")
 			elements = tree.xpath('//table[@class="list"]//tr')
-			logging.info(str(len(elements)))
+			for rowindex in range(len(elements)):
+				if len(elements[rowindex])>3 and elements[rowindex][1].text is not None and elements[rowindex][2].text is not None:
+					logging.info("Date: " + elements[rowindex][1].text + " Time: " + elements[rowindex][2].text
+						+ " Location: " + elements[rowindex][3][0].text + 
+						" Home: " + elements[rowindex][4].text + 
+						" Visitor: " + elements[rowindex][5].text)
+
 
 app = webapp2.WSGIApplication([('/crontask/scrape', Load)],debug=True)
 
