994403a1501859920784bfc8cddc5d5f57efc898
John Sextro <John.Sextro@gmail.com>
2013-03-28 12:14:28 -0500

    Successfully returning a Coaches

---
+++ SaintsServer/coach_service.py
@@ -0,0 +1,36 @@
+from protorpc import messages
+from protorpc import remote
+from protorpc.wsgi import service
+from team import Team
+import logging
+
+package = 'SaintsSchedule'
+
+class Coach(messages.Message):
+
+	team_id = messages.StringField(1, required=True)
+	name = messages.StringField(2, required=True)
+	school = messages.StringField(3)
+
+class Coaches(messages.Message):
+	coaches = messages.MessageField(Coach, 1, repeated=True)
+
+# Create the request string containing the user's name
+class CoachRequest(messages.Message):
+	school = messages.StringField(1, required=False)
+
+# Create the RPC service to exchange messages
+class CoachService(remote.Service):
+
+	@remote.method(CoachRequest, Coaches)
+	def coach(self, request):
+		logging.info("Entering service method")
+		t = Team()
+		coaches = []
+		for team in t.getCoaches():
+			coach = Coach(team_id=team.teamId, name=team.coach, school=team.school)
+			coaches.append(coach)
+		return Coaches(coaches=coaches)
+
+# Map the RPC service and path (/schedule)
+app = service.service_mappings([('/coach.*', CoachService)])
--- SaintsServer/team.py
+++ SaintsServer/team.py
@@ -4,19 +4,19 @@ import logging
 package = 'SaintsSchedule'
 
 class Team(db.Model):
-    teamId = db.StringProperty()
-    coach = db.StringProperty()
-    school = db.StringProperty()
-    grade = db.IntegerProperty()
-    year = db.IntegerProperty()
-    schedule = db.TextProperty()
+	teamId = db.StringProperty()
+	coach = db.StringProperty()
+	school = db.StringProperty()
+	grade = db.IntegerProperty()
+	year = db.IntegerProperty()
+	schedule = db.TextProperty()
 
-    # schedule contains json like this below
-    # {"games": [{"game_date": "4/1/2013", "time": "1:00 PM", "home": "St. J & A", "away": "ICD", location": "St. Joes"}]}
+	# schedule contains json like this below
+	# {"games": [{"game_date": "4/1/2013", "time": "1:00 PM", "home": "St. J & A", "away": "ICD", location": "St. Joes"}]}
 
-    def getGames(self, teamId):
+	def getGames(self, teamId):
 		team = Team()
-		games = ""
+		games = ''
 		q = db.Query(Team)
 		q = Team.all()
 		q.filter("teamId =", str(teamId))
@@ -24,3 +24,13 @@ class Team(db.Model):
 			team = q.get()
 			games = str(team.schedule)
 		return games
+
+
+	def getCoaches(self):
+		logging.info("entering model method")
+		team = Team()
+		q = db.Query(Team, projection=('teamId', 'school', 'coach'))
+		q = Team.all()
+		q.order("school")
+		q.order("coach")
+		return q.run()
