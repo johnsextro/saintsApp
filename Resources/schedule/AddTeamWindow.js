fa1ad51c35f8931adee7b21f26b20e3b042723bc
John Sextro <John.Sextro@gmail.com>
2013-03-29 18:19:41 -0500

    Now pulling coaches from the service and displaying them on a new window.  Need to wire up the team_id to the picker and wire the add team button up to add the team to the device.

---
+++ Resources/schedule/AddTeamWindow.js
@@ -0,0 +1,67 @@
+function AddTeamWindow() {
+	var self = Ti.UI.createWindow({
+		title:'Add Team',
+		backgroundColor:'black'
+	});
+
+	var url = "http://localhost:8080/coach/CoachService.coach";
+	var data = [];
+	var json;
+	var btnAddTeam = Titanium.UI.createButton({
+		title:'Add Team',
+		height:40,
+		width:200
+	});
+	
+	var xhr = Ti.Network.createHTTPClient({
+    onload: function() {
+
+		json = JSON.parse(this.responseText);
+		if (json.coaches != "") {
+			
+			//replace this with a for loop to load all the games rather than just one.
+			//{"games": [{"game_date": "Sat, 01/05/2013", "time": "1:00 PM", "home": "St_Cletus-Schultehenrich (O)", "away": "SJC-Edmunds (C)", "location": "St_Cletus"}]}
+			for (i = 0; i < json.coaches.length; i++) {
+				coach = json.coaches[i];
+				data.push({title: coach.name, hasChild:false, test:''});
+			}	
+		} else {
+			data.push({title: "No Coaches Found", hasChild:false, test:''});
+		}
+		
+		// add table view to the window
+		var pckrTeams = Ti.UI.createPicker();
+		pckrTeams.add(data); 
+		pckrTeams.selectionIndicator = true;
+		self.add(label1);
+		self.add(pckrTeams);
+		pckrTeams.setSelectedRow(0,1,true);
+		btnAddTeam.setTop(400);
+		self.add(btnAddTeam);			
+    },
+	onerror: function(e) {
+		Ti.API.error("STATUS: " + this.status);
+		Ti.API.error("TEXT:   " + this.responseText);
+		Ti.API.error("ERROR:  " + e.error);
+		alert('There was an error retrieving the remote data. Try again.');
+	    },
+	    timeout:5000
+	});
+	
+	var label1 = Ti.UI.createLabel({
+	  color: 'white',
+	  text: 'Choose a Team to Add',
+	  font:{fontSize:14},
+	  top: 10,
+	  width: 'auto', height: 'auto'
+	});
+	
+
+	var params = '';
+	xhr.open("POST", url);
+	xhr.setRequestHeader('Content-Type','application/json')
+	xhr.send(params);
+	return self;
+};
+
+module.exports = AddTeamWindow;
--- Resources/ui/common/TeamsWindow.js
+++ Resources/ui/common/TeamsWindow.js
@@ -3,6 +3,7 @@ function TeamsWindow(title) {
 		title:title,
 		backgroundColor:'white'
 	});
+	AddTeamWindow = require('schedule/AddTeamWindow');
 	Ti.App.Properties.setList('Teams', [['5766', 'SJC-Edmunds'], ['5767', 'Test Team']]);
 	var teams = Ti.App.Properties.getList('Teams'); 
 	
@@ -38,7 +39,7 @@ function TeamsWindow(title) {
 	
 	var btnAddTeam = Titanium.UI.createButton({title:'Add'});
 	btnAddTeam.addEventListener('click', function(e) {
-		win1 = AddTeam();
+		win1 = new AddTeamWindow();
 		win1.open();
 	});
 	
@@ -46,79 +47,4 @@ function TeamsWindow(title) {
 
 	return self;
 };
-
-function AddTeam() {
-	var self = Ti.UI.createWindow({
-		title:'Add Team',
-		backgroundColor:'white'
-	});
-
-	var url = "http://localhost:8080/schedule/ScheduleService.schedule";
-	var data = []
-	var json, schedule, fighters, games;
-	
-	var xhr = Ti.Network.createHTTPClient({
-    onload: function() {
-
-		json = JSON.parse(this.responseText);
-		if (json.schedule != "") {
-			json = JSON.parse(json.schedule);
-			//replace this with a for loop to load all the games rather than just one.
-			//{"games": [{"game_date": "Sat, 01/05/2013", "time": "1:00 PM", "home": "St_Cletus-Schultehenrich (O)", "away": "SJC-Edmunds (C)", "location": "St_Cletus"}]}
-			for (i = 0; i < json.games.length; i++) {
-				game = json.games[i];
-				data.push({title: game.game_date + ' ' + game.time + ' at ' + game.location, hasChild:false, test:''});
-				// ' ' + game.home + ' vs. ' + game.away +
-			}	
-		} else {
-			data.push({title: "No Games Found", hasChild:false, test:''});
-		}
-		
-		// create table view
-		for (var i = 0; i < data.length; i++ ) { data[i].color = '#000'; data[i].font = {fontWeight:'bold'} };
-		var tableview = Titanium.UI.createTableView({
-			data:data
-		});
-	
-		// add table view to the window
-		self.add(tableview);
-		// spinner.hide();
-    },
-	onerror: function(e) {
-		Ti.API.error("STATUS: " + this.status);
-		Ti.API.error("TEXT:   " + this.responseText);
-		Ti.API.error("ERROR:  " + e.error);
-		alert('There was an error retrieving the remote data. Try again.');
-	    },
-	    timeout:5000
-	});
-	
-	var label1 = Ti.UI.createLabel({
-	  color: 'black',
-	  text: 'Choose a Team to Add',
-	  font:{fontSize:14},
-	  top: 10,
-	  width: 'auto', height: 'auto'
-	});
-	
-	var pckrdata = [];
-	pckrdata[0]=Ti.UI.createPickerRow({title:'SJC-Edmunds',custom_item:'b'});
-	pckrdata[1]=Ti.UI.createPickerRow({title:'SJC-Sextro',custom_item:'s'});
-	pckrdata[2]=Ti.UI.createPickerRow({title:'SJC-Hall',custom_item:'m'});
-	pckrdata[3]=Ti.UI.createPickerRow({title:'SJC-Haefner',custom_item:'g'});
-	var pckrTeams = Ti.UI.createPicker();
-	pckrTeams.add(pckrdata); 
-	pckrTeams.selectionIndicator = true;
-		
-	self.add(label1);
-	self.add(pckrTeams);
-	pckrTeams.setSelectedRow(0,1,true);
-	
-	// var params = '{"team_id": "5766"}';
-	// xhr.open("POST", url);
-	// xhr.setRequestHeader('Content-Type','application/json')
-	// xhr.send(params);
-	return self;
-};
-
 module.exports = TeamsWindow;
